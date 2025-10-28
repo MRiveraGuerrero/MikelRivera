42a8eba99af97c5933be2bc1a757f29ec30ff842
MRiveraGuerrero <mikelrg2003@gmail.com>
2025-10-28 21:06:32 +0100

    separado correctamente

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,34 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Portfolio1App from "./FirstPortfolio/Portfolio1App";
-import Portfolio2App from "./SecondPortfolio/Portfolio2App";
 import PortfolioSelector from "./PortfolioSelector";
 
-function App() {
+const Portfolio1App = React.lazy(() => import("./FirstPortfolio/Portfolio1App.jsx"));
+const Portfolio2App = React.lazy(() => import("./SecondPortfolio/Portfolio2App.jsx"));
+
+export default function App() {
   return (
     <BrowserRouter>
-    
-      <Routes>
-        <Route
-          path="/portfolio1/*"
-          element={
-            <div className="portfolio1">
-              <Portfolio1App />
-            </div>
-          }
-        />
-        <Route path="/portfolios" element={<PortfolioSelector />} />
-        <Route
-          path="/*"
-          element={
-            <div className="portfolio2">
-              <Portfolio2App />
-            </div>
-          }
-        />
-      </Routes>
+      <Suspense fallback={<div>Cargando...</div>}>
+        <Routes>
+          <Route path="/portfolios" element={<PortfolioSelector />} />
+          <Route path="/portfolio1/*" element={<Portfolio1App />} />
+          <Route path="/*" element={<Portfolio2App />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
-
-export default App;
