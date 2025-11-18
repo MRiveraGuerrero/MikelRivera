5be63e9cc8ed684709770b1b902a87d50c65c150
MRiveraGuerrero <mikelrg2003@gmail.com>
2025-11-18 22:08:36 +0100

    sistema solar cabe

--- my-portfolio/src/Home/Header.jsx
+++ my-portfolio/src/Home/Header.jsx
@@ -2,15 +2,11 @@ import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>M</div>
-
-      <nav className={styles.nav}>
-        <a href="/">Home</a>
-        <a href="/about">Sobre mí</a>
-        <a href="/lab">Lab</a>
-        <a href="/portfolio">Portfolio</a>
-      </nav>
-    </header>
+    <div className={styles.floatingNav}>
+      <a href="/">Home</a>
+      <a href="/about">Sobre mí</a>
+      <a href="/lab">Lab</a>
+      <a href="/portfolio">Portfolio</a>
+    </div>
   );
 }
