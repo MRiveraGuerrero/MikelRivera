ef359dcc7443912fadc02980a492294df3d2c1a6
MRiveraGuerrero <mikelrg2003@gmail.com>
2025-11-18 22:28:46 +0100

    ya se puede seleccionar un planeta

--- my-portfolio/src/Home/PlanetZoomPanel.jsx
+++ my-portfolio/src/Home/PlanetZoomPanel.jsx
@@ -2,18 +2,27 @@ import styles from "./PlanetZoomPanel.module.css";
 
 export default function PlanetZoomPanel({ item, onClose }) {
   return (
-    <div className={styles.overlay} onClick={onClose}>
-      <div className={styles.panel} onClick={(e) => e.stopPropagation()}>
-        
+    <div className={styles.container}>
+
+      {/* PLANETA ANCLADO A LA IZQUIERDA */}
+      <div className={styles.planetDock}>
+        <img src={item.img} alt={item.label} className={styles.planetImg} />
+      </div>
+
+      {/* PANEL A LA DERECHA */}
+      <div className={styles.panel}>
         <h2 className={styles.title}>{item.label}</h2>
-        <p className={styles.text}>{item.description}</p>
+        <p className={styles.desc}>{item.description}</p>
 
         <a href={item.link} className={styles.button}>
           Entrar al planeta
         </a>
 
-        <button className={styles.close} onClick={onClose}>Cerrar</button>
+        <button className={styles.close} onClick={onClose}>
+          Cerrar
+        </button>
       </div>
+
     </div>
   );
 }
